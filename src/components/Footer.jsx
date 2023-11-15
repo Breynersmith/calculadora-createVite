3886de0c50043cb655d31d3ee80e886472509530
Breyner Ustariz <breynersmithustariz@mail.com>
2023-11-14 20:36:09 -0500

    calculadora lista

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>
+        by <a href="https://github.com/Breynersmith">Breyner Ustariz</a>
+      </p>
+      <div className="socials">
+        <a
+          href="https://github.com/Breynersmith"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fa-brands fa-github"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/breynerustariz/" target="_blank" rel="noopener noreferrer">
+          <i className="fa-brands fa-linkedin"></i>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+
+function Header() {
+  return (
+    <div className="header">
+      <nav>
+        <h1>Calculator</h1>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
